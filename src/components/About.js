3c881dc27adcb71e320dd65dfda37ac2a9e8f337
Ndiema-S <you@example.com>
2022-12-06 11:41:22 +0300

    Add about

---
+++ src/components/About.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div className='about' id='about'>
+            <div className='a'>
+                <p style={{ fontSize: "50px" }}>Lorem ipsum dolor sit amet consectetur adipisicing elit of local businesses, so it's easy to find jobs that are right for you.
+                    Have an idea of what Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur adipisicing elit.Ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+            </div>
+            <div id='scouter'>
+                <img src="https://bookscouter.com/blog/wp-content/uploads/2016/09/aboutbookscouter.png" alt="Top 10 Best Comic Books of All Time" />
+            </div>
+        </div>
+    )
+}
+
+export default About
+
+
+
