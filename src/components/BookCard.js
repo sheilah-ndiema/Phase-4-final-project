64e41c470b4df6e5f5865dff7456e4b5f59bc480
Ndiema-S <you@example.com>
2022-12-06 11:42:04 +0300

    Add book card

---
+++ src/components/BookCard.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+function BookCard({ book, handleDeleteBook }) {
+    const { name, image, price } = book;
+
+    const [isInStock, setIsInStock] = useState(true);
+
+    function handleToggleStock() {
+        setIsInStock((isInStock) => !isInStock);
+    }
+
+    function handleDelete() {
+        fetch(`http://localhost:4000/books/${book.id}`, {
+            method: "DELETE",
+        }).then(() => handleDeleteBook(book.id))
+    }
+
+    return (
+        <li className="card">
+            <img src={image} alt={name} />
+            <h4>{name}</h4>
+            <p>Price: {price}</p>
+            {isInStock ? (
+                <button className="primary" onClick={handleToggleStock}>
+                    In Stock
+                </button>
+            ) : (
+                <button onClick={handleToggleStock}>Out of Stock</button>
+            )}
+            <button className="primary" onClick={handleDelete}>
+                Delete
+            </button>
+        </li>
+    );
+}
+
+export default BookCard;
