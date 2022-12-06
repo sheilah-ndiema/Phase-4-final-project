8e0a0c0a1f61f5cb19105db1152d4e2397dd51c0
Ndiema-S <you@example.com>
2022-12-06 11:43:21 +0300

    Add Home.js

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+        <header>
+            <h1>
+                <img className="logo" src='./images/good.png' alt="logo" />
+
+
+            </h1>
+        </header>
+    );
+}
+
+export default Header;
