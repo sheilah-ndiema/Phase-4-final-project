b87def0438e038c2b9e7072b69fe31804a8143f2
Ndiema-S <you@example.com>
2022-12-07 15:04:19 +0300

    app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,40 @@
 import Header from "./Header";
 import BookPage from "./BookPage";
+import { useEffect, useState } from 'react'
 import NavBar from "./NavBar"
-import { Routes, Route } from "react-router-dom";
+import LoginForm from "./Login";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Home from "./Home"
 import About from "./About";
+import SignupForm from "./SignupForm.js";
 
 
 function App() {
+    const [user, setUser] = useState(null)
+    const navigate = useNavigate()
+    function onLogin(user) {
+        setUser(user)
+        return navigate("/bookPage")
+    }
+    useEffect(() => {
+        fetch("/me").then(res => {
+            if (res.ok) {
+                res.json().then(data => setUser(data))
+            }
+        })
+    }, [])
     return (
         <div className="app">
             <Header />
             <NavBar />
             <Routes>
-                <Route exact path='/home' element={<Home />} />
+                <Route exact path='/home' element={<Home user={user} setUser={setUser} />} />
+                <Route exact path='/' element={<Home user={user} setUser={setUser} />} />
+                <Route exact path='/signup' element={<SignupForm onLogin={onLogin} />} />
+                <Route exact path='/login' element={<LoginForm onLogin={onLogin} />} />
                 <Route exact path='/about' element={<About />} />
-                <Route exact path='/bookPage' element={<BookPage />} />
+                <Route exact path='/bookPage' element={<BookPage user={user} />} />
+
             </Routes>
 
 
