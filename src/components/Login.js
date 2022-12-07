65ef247232cda30c73d13ad99cc83adf7d2fd222
Ndiema-S <you@example.com>
2022-12-07 15:05:25 +0300

    update login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom"
 
-function Login({ onLogin }) {
+function LoginForm({ onLogin }) {
     const [username, setUsername] = useState("");
-
+    const [password, setPassword] = useState("");
+    const [errors, setErrors] = useState([]);
     function handleSubmit(e) {
         e.preventDefault();
         fetch("/login", {
@@ -10,22 +12,43 @@ function Login({ onLogin }) {
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({ username }),
-        })
-            .then((r) => r.json())
-            .then((user) => onLogin(user));
+            body: JSON.stringify({ username, password }),
+        }).then((r) => {
+            if (r.ok) {
+                r.json().then((user) => onLogin(user));
+            } else {
+                r.json().then((err) => setErrors(err.errors));
+            }
+        });
     }
 
     return (
         <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username</label>
             <input
                 type="text"
+                id="username"
+                autoComplete="off"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
             />
-            <button type="submit">Login</button>
+            <label htmlFor="password">Password</label>
+            <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit">
+                {"Login"}
+            </button>
+            <p>Or <NavLink className="btn" to='/signup'>create account</NavLink></p>
+
+            {errors.map((err) => (
+                <p style={{ color: "red" }} key={err}>{err}</p>
+            ))}
         </form>
     );
 }
 
-export default Login;
+export default LoginForm;
