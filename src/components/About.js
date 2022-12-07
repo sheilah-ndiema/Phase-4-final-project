796563149bda874d07ca372fab3a4c803d178f41
Ndiema-S <you@example.com>
2022-12-07 21:09:31 +0300

    update about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,26 @@
 import React from 'react'
+import Socials from "./Socials";
+
 
 function About() {
     return (
-        <div className='about' id='about'>
-            <div className='a'>
-                <p style={{ fontSize: "50px" }}>Lorem ipsum dolor sit amet consectetur adipisicing elit of local businesses, so it's easy to find jobs that are right for you.
-                    Have an idea of what Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur adipisicing elit.Ipsum dolor sit amet consectetur adipisicing elit.
-                </p>
+
+        <div>
+            <div className='about' id='about'>
+                <div className='a'>
+                    <p style={{ fontSize: "50px" }}> Get access to the hottest book flipping deals based
+                        on our data! We've worked out the way to find and immediately deliver
+                        this information to you; your job is to react fast and earn your
+                        reselling profit without effort!contact us in any of our social
+                        platforms for more information
+
+                    </p>
+                </div>
+                <div id='scouter'>
+                    <img src="https://bookscouter.com/blog/wp-content/uploads/2016/09/aboutbookscouter.png" alt="Top 10 Best Comic Books of All Time" />
+                </div>
             </div>
-            <div id='scouter'>
-                <img src="https://bookscouter.com/blog/wp-content/uploads/2016/09/aboutbookscouter.png" alt="Top 10 Best Comic Books of All Time" />
-            </div>
+            <Socials />
         </div>
     )
 }
